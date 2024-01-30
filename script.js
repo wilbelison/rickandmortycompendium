0e6a650e948b8ca9af8f146e69267a2ee9834881
Wilbelison Junior <meustudio@gmail.com>
2024-01-30 17:57:03 -0300

    script.js and getting page

---
+++ script.js
@@ -0,0 +1,25 @@
+const urlParams = new URLSearchParams(window.location.search);
+const category = urlParams.get("category") || "characters";
+const page = urlParams.get("page") || 1;
+
+setPage = function (info) {
+  console.log(info);
+};
+
+if (category == "locations") {
+} else if (category == "episodes") {
+} else {
+  const data = fetch("https://rickandmortyapi.com/api/character/?page=" + page)
+    .then((response) => response.json())
+    .then((response) => (content = response))
+    .then(function () {
+      const template = document.querySelector("#template-character").innerHTML;
+      const container = document.querySelector("#content");
+
+      contentHtml = Mustache.render(template, content.results);
+
+      container.innerHTML = contentHtml;
+
+      setPage(content.info);
+    });
+}
