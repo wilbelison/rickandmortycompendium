b0c5022ab31988525d4d73d656e6c9738ee1fa6f
Wilbelison Junior <meustudio@gmail.com>
2024-02-01 20:23:19 -0300

    aula api: exemplo 1

---
+++ api.js
@@ -0,0 +1,7 @@
+console.log("1) Escolher sabor da pizza;");
+console.log("2) Pedir pizza;");
+console.log("3) Pagar pizza;");
+setTimeout(() => {
+  console.log("4) Levar pizza para casa.");
+}, 2000);
+console.log("Ler uma página  de um livro;");
