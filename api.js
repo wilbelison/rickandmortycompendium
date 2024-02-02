c5f0eab71805535feafc018945716877dfdb780f
Wilbelison Junior <meustudio@gmail.com>
2024-02-01 21:10:16 -0300

    exemplo mudando bandeira

--- api.js
+++ api.js
@@ -1,5 +1,8 @@
-const minhaPromise = fetch('https://viacep.com.br/ws/08340146/json').then((response) => {
+const minhaPromise = fetch('https://restcountries.com/v3.1/name/brazil').then((response) => {
     return response.json();
-}).then((cep) => {
-    console.log(cep.logradouro);
+}).then((country) => {
+    const srcBandeira = country[0].flags.png;
+    let bandeira = document.querySelector('img');
+    bandeira.setAttribute('src', srcBandeira);
+
 });
