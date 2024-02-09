a7fa70d6a2a344877afadf6950a254aa9a652423
Wilbelison Junior <meustudio@gmail.com>
2024-02-08 23:38:07 -0300

    pagination

--- script.js
+++ script.js
@@ -2,30 +2,6 @@ const urlParams = new URLSearchParams(window.location.search);
 const filterCategory = urlParams.get("filter-category") || "character";
 const filterPage = urlParams.get("filter-page") || 1;
 
-
-setPage = function (info) {
-  const menu = document.querySelector("#menu");
-  menu.querySelector("a." + filterCategory).classList.add("active");
-
-  const pagination = document.querySelector("#pagination");
-
-  const nextButton = pagination.querySelector(".next");
-  const prevButton = pagination.querySelector(".prev");
-
-  const selectPage = pagination.querySelector("#page");
-
-  selectPage.addEventListener("change", () => {
-    alert(selectPage.value);
-  });
-
-  nextButton.addEventListener("click", () => {
-    alert("next");
-  });
-  prevButton.addEventListener("click", () => {
-    alert("prev");
-  });
-};
-
 Handlebars.registerHelper('for', function(from, to, incr, block) {
   var accum = '';
   for(var i = from; i < to + 1; i += incr)
@@ -39,16 +15,42 @@ const data = fetch(
   .then((response) => response.json())
   .then((response) => (content = response))
   .then(function () {
+
     console.log(content);
+
     const contentTemplate = Handlebars.compile(
       document.querySelector("#template-" + filterCategory).innerHTML
     );
+
     const paginationTemplate = Handlebars.compile(
       document.querySelector("#template-pagination").innerHTML
     );
+
     const container = document.querySelector("#contents");
     const pagination = document.querySelector("#pagination");
+
     container.innerHTML = contentTemplate(content);
     pagination.innerHTML = paginationTemplate(content);
-    setPage(content.info);
+    
+    const menu = document.querySelector("#menu");
+    menu.querySelector("a." + filterCategory).classList.add("active");
+  
+  
+    const nextButton = pagination.querySelector(".next");
+    const prevButton = pagination.querySelector(".prev");
+  
+    const selectPage = pagination.querySelector("#page");
+  
+    selectPage.addEventListener("change", () => {
+      alert(selectPage.value);
+    });
+  
+    nextButton.addEventListener("click", () => {
+      alert("next");
+    });
+  
+    prevButton.addEventListener("click", () => {
+      alert("prev");
+    });
+
   });
