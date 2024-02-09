3dd12d133d029e32f369d275d86ccfca18550cc5
Wilbelison Junior <meustudio@gmail.com>
2024-02-09 08:05:36 -0300

    characters filters funcionando

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 const urlParams = new URLSearchParams(window.location.search);
 const filterCategory = urlParams.get("category") || "character";
 const filterPage = urlParams.get("page") || 1;
+urlParams.set("category", filterCategory);
+urlParams.set("page", filterPage);
 
-Handlebars.registerHelper("repeat", function (n, block) {
-  var accum = "";
-  for (var i = 1; i < n + 1; ++i) accum += block.fn(i);
-  return accum;
+console.log(urlParams.toString());
+
+Handlebars.registerHelper("repeat", function (n, options) {
+  var out = "";
+  for (var i = 1; i < n + 1; ++i) out += options.fn(i);
+  return out;
 });
 
 const data = fetch(
-  "https://rickandmortyapi.com/api/" + filterCategory + "?page=" + filterPage
+  "https://rickandmortyapi.com/api/" + filterCategory + "?" + urlParams.toString()
 )
   .then((response) => response.json())
   .then((response) => (content = response))
   .then(function () {
+
     console.log(content);
+
+    const menu = document.querySelector("#menu");
+    menu.querySelector("a." + filterCategory).classList.add("active");
 
     const contentTemplate = Handlebars.compile(
       document.querySelector("#template-" + filterCategory).innerHTML
@@ -26,31 +34,46 @@ const data = fetch(
 
     const container = document.querySelector("#contents");
     const pagination = document.querySelector("#pagination");
+    const filters = document.querySelector("#filters");
 
     container.innerHTML = contentTemplate(content);
     pagination.innerHTML = paginationTemplate(content);
 
-    const menu = document.querySelector("#menu");
-    menu.querySelector("a." + filterCategory).classList.add("active");
+    const selectPage = pagination.querySelector("#page");
+    
+    if(selectPage) {
+
+      selectPage.addEventListener("change", () => {
+        const newPage = urlParams.set("page", selectPage.value);
+        console.log(newPage);
+        console.log(newPage.toString());
+        window.location.href = newPage.toString();
+      });
+   
+      selectPage
+        .querySelector("option:nth-child(" + filterPage + ")")
+        .setAttribute('selected', 'selected');
+
+    }
 
     const nextButton = pagination.querySelector(".next");
+
+    if(nextButton) {
+    
+      nextButton.addEventListener("click", () => {
+        alert("next");
+      });
+
+    }
+
     const prevButton = pagination.querySelector(".prev");
+      
+    if(prevButton) {
 
-    const selectPage = pagination.querySelector("#page");
+      prevButton.addEventListener("click", () => {
+        alert("prev");
+      });
 
-    selectPage.addEventListener("change", () => {
-      alert(selectPage.value);
-    });
+    }
 
-    selectPage
-      .querySelector("option:nth-child(" + filterPage + ")")
-      .setAttribute('selected', 'selected');
-
-    nextButton.addEventListener("click", () => {
-      alert("next");
-    });
-
-    prevButton.addEventListener("click", () => {
-      alert("prev");
-    });
   });
