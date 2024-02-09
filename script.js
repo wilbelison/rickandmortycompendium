baee55e09e327254e7a2e226ebb7e3232e2a3715
Wilbelison Junior <meustudio@gmail.com>
2024-02-09 06:02:42 -0300

    pagina selecionada

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 const urlParams = new URLSearchParams(window.location.search);
-const filterCategory = urlParams.get("filter-category") || "character";
-const filterPage = urlParams.get("filter-page") || 1;
+const filterCategory = urlParams.get("category") || "character";
+const filterPage = urlParams.get("page") || 1;
 
-Handlebars.registerHelper('for', function(from, to, incr, block) {
-  var accum = '';
-  for(var i = from; i < to + 1; i += incr)
-      accum += block.fn(i);
+Handlebars.registerHelper("repeat", function (n, block) {
+  var accum = "";
+  for (var i = 1; i < n + 1; ++i) accum += block.fn(i);
   return accum;
 });
 
 const data = fetch(
-  "https://rickandmortyapi.com/api/" + filterCategory + '?page=' + filterPage
+  "https://rickandmortyapi.com/api/" + filterCategory + "?page=" + filterPage
 )
   .then((response) => response.json())
   .then((response) => (content = response))
   .then(function () {
-
     console.log(content);
 
     const contentTemplate = Handlebars.compile(
@@ -31,26 +29,28 @@ const data = fetch(
 
     container.innerHTML = contentTemplate(content);
     pagination.innerHTML = paginationTemplate(content);
-    
+
     const menu = document.querySelector("#menu");
     menu.querySelector("a." + filterCategory).classList.add("active");
-  
-  
+
     const nextButton = pagination.querySelector(".next");
     const prevButton = pagination.querySelector(".prev");
-  
+
     const selectPage = pagination.querySelector("#page");
-  
+
     selectPage.addEventListener("change", () => {
       alert(selectPage.value);
     });
-  
+
+    selectPage
+      .querySelector("option:nth-child(" + filterPage + ")")
+      .setAttribute('selected', 'selected');
+
     nextButton.addEventListener("click", () => {
       alert("next");
     });
-  
+
     prevButton.addEventListener("click", () => {
       alert("prev");
     });
-
   });
