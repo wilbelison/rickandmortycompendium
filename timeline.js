df068a203f4fcdb61bfe657912f29402b9496cd5
Wilbelison Junior <meustudio@gmail.com>
2024-02-17 08:57:41 -0300

    timeline awaits

--- timeline.js
+++ timeline.js
@@ -6,84 +6,89 @@ data.locations = {};
 
 const urlParseInt = (str) => parseInt(str.split(/[/]+/).pop());
 
-const dataCharacters = fetch("https://rickandmortyapi.com/api/character")
-  .then((response) => response.json())
-  .then((response) => {
-    for (page = 1; page <= response.info.pages; page++) {
-      const pageData = fetch(
-        "https://rickandmortyapi.com/api/character?page=" + page
-      )
-        .then((response) => response.json())
-        .then((response) => {
-          response.results.forEach((result) => {
+const fetchRickAndMorty = async () => {
 
-            result.episode = result.episode.map((episode) =>
-              urlParseInt(episode)
-            );
+  const dataCharacters = await fetch("https://rickandmortyapi.com/api/character")
+    .then((response) => response.json())
+    .then((response) => {
+      for (page = 1; page <= response.info.pages; page++) {
+        const pageData = fetch(
+          "https://rickandmortyapi.com/api/character?page=" + page
+        )
+          .then((response) => response.json())
+          .then((response) => {
+            response.results.forEach((result) => {
+              result.episode = result.episode.map((episode) =>
+                urlParseInt(episode)
+              );
 
-            delete result.created;
-            delete result.image;
-            delete result.url;
+              delete result.created;
+              delete result.image;
+              delete result.url;
 
-            result.location = (result.location.url != '') ? urlParseInt(result.location.url) : null ;
-            result.origin = (result.origin.url != '') ? urlParseInt(result.origin.url) : null ;
+              result.location =
+                result.location.url != ""
+                  ? urlParseInt(result.location.url)
+                  : null;
+              result.origin =
+                result.origin.url != "" ? urlParseInt(result.origin.url) : null;
 
-            data.characters[result.id] = result;
-
+              data.characters[result.id] = result;
+            });
           });
-        });
-    }
-  });
+      }
+    });
 
-const dataEpisodes = fetch("https://rickandmortyapi.com/api/episode")
-  .then((response) => response.json())
-  .then((response) => {
-    for (page = 1; page <= response.info.pages; page++) {
-      const pageData = fetch(
-        "https://rickandmortyapi.com/api/episode?page=" + page
-      )
-        .then((response) => response.json())
-        .then((response) => {
-          response.results.forEach((result) => {
+  const dataEpisodes = await fetch("https://rickandmortyapi.com/api/episode")
+    .then((response) => response.json())
+    .then((response) => {
+      for (page = 1; page <= response.info.pages; page++) {
+        const pageData = fetch(
+          "https://rickandmortyapi.com/api/episode?page=" + page
+        )
+          .then((response) => response.json())
+          .then((response) => {
+            response.results.forEach((result) => {
+              result.characters = result.characters.map((character) =>
+                urlParseInt(character)
+              );
 
-            result.characters = result.characters.map((character) =>
-              urlParseInt(character)
-            );
+              delete result.created;
+              delete result.url;
 
-            delete result.created;
-            delete result.url;
-
-            data.episodes[result.id] = result;
-
+              data.episodes[result.id] = result;
+            });
           });
-        });
-    }
-  });
+      }
+    });
 
-const dataLocations = fetch("https://rickandmortyapi.com/api/location")
-  .then((response) => response.json())
-  .then((response) => {
-    for (page = 1; page <= response.info.pages; page++) {
-      const pageData = fetch(
-        "https://rickandmortyapi.com/api/location?page=" + page
-      )
-        .then((response) => response.json())
-        .then((response) => {
-          response.results.forEach((result) => {
+  const dataLocations = await fetch("https://rickandmortyapi.com/api/location")
+    .then((response) => response.json())
+    .then((response) => {
+      for (page = 1; page <= response.info.pages; page++) {
+        const pageData = fetch(
+          "https://rickandmortyapi.com/api/location?page=" + page
+        )
+          .then((response) => response.json())
+          .then((response) => {
+            response.results.forEach((result) => {
+              result.residents = result.residents.map((character) =>
+                urlParseInt(character)
+              );
 
-            result.residents = result.residents.map((character) =>
-              urlParseInt(character)
-            );
+              delete result.created;
+              delete result.url;
 
-            delete result.created;
-            delete result.url;
-
-            data.locations[result.id] = result;
-
+              data.locations[result.id] = result;
+            });
           });
-        });
-    }
-  });
+      }
+    });
+};
 
-console.log(data.locations);
+fetchRickAndMorty().then(() => {
+    console.log('terminou');
+    console.log(data);
+})
 
+console.log('nao terminou');
