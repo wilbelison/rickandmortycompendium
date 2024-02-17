f4db26fea942c41a4fd32ae52f399a96413600ea
Wilbelison Junior <meustudio@gmail.com>
2024-02-17 08:41:02 -0300

    results filtred (delete imagens, createds and urls)

--- timeline.js
+++ timeline.js
@@ -16,10 +16,20 @@ const dataCharacters = fetch("https://rickandmortyapi.com/api/character")
         .then((response) => response.json())
         .then((response) => {
           response.results.forEach((result) => {
+
             result.episode = result.episode.map((episode) =>
               urlParseInt(episode)
             );
+
+            delete result.created;
+            delete result.image;
+            delete result.url;
+
+            result.location = (result.location.url != '') ? urlParseInt(result.location.url) : null ;
+            result.origin = (result.origin.url != '') ? urlParseInt(result.origin.url) : null ;
+
             data.characters[result.id] = result;
+
           });
         });
     }
@@ -35,10 +45,16 @@ const dataEpisodes = fetch("https://rickandmortyapi.com/api/episode")
         .then((response) => response.json())
         .then((response) => {
           response.results.forEach((result) => {
+
             result.characters = result.characters.map((character) =>
               urlParseInt(character)
             );
+
+            delete result.created;
+            delete result.url;
+
             data.episodes[result.id] = result;
+
           });
         });
     }
@@ -54,13 +70,20 @@ const dataLocations = fetch("https://rickandmortyapi.com/api/location")
         .then((response) => response.json())
         .then((response) => {
           response.results.forEach((result) => {
+
             result.residents = result.residents.map((character) =>
               urlParseInt(character)
             );
+
+            delete result.created;
+            delete result.url;
+
             data.locations[result.id] = result;
+
           });
         });
     }
   });
 
-console.log(data);
+console.log(data.locations);
+
